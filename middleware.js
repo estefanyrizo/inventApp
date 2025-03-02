1e6c75270574af68f0fa2a9be8c969a46a874c1b
estefanyrizo <estefanyrizom@gmail.com>
2025-03-02 01:44:37 -0600

    feat: agregar verificación de token JWT en la ruta /verifyToken

--- middleware.js
+++ middleware.js
@@ -38,4 +38,39 @@ module.exports = (req, res, next) => {
     return res.status(200).json({ token }); 
   }
 
+// Verificar si el token es valido
+  const verifyToken = (token) => {
+    try {
+      // Verifica la firma del token
+      const decoded = jwt.verify(token, SECRET_KEY);
+      return true;
+    } catch (error) {
+      // Manejo de errores específicos
+      if (error.name === 'TokenExpiredError') {
+        console.error('Token expirado:', error.message);
+      } else if (error.name === 'JsonWebTokenError') {
+        console.error('Token inválido:', error.message);
+      } else {
+        console.error('Error al verificar el token:', error);
+      }
+      return false;
+    }
+  }
+
+  if (url === '/verifyToken' && method === 'POST') {
+    const authHeader = headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Token no proporcionado' });
+    }
+
+    const token = authHeader.split(' ')[1]; // Extrae el token del encabezado
+
+    if (!verifyToken(token)) {
+      return res.status(401).json({ message: 'Token inválido o expirado' });
+    }
+
+    return res.status(200).json({message: 'Token valido'});
+  }
+
 }
